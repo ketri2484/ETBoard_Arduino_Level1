77d3019ca45ab479ec921080db00083a7cf253c0
kpej <kpej@ketri.re.kr>
2023-03-14 16:56:00 +0900

    Modified to comment lenght

--- DigitalReadSerial_ET_Board/DigitalReadSerial_ET_Board.ino
+++ DigitalReadSerial_ET_Board/DigitalReadSerial_ET_Board.ino
@@ -6,6 +6,7 @@
  * Created Date : 2022.04.21
  * Reference    : 
  * Modified     : 2022.04.21 : LEH : 헤더 수정, 소스 크린징
+ * Modified     : 2023.03.14 : PEJ : 주석 길이 수정
 ******************************************************************************************/
 
 int pushButton = D6;                          // ET보드의 빨간 버튼 : D6
@@ -15,7 +16,7 @@ void setup() {
 }
 
 void loop() {
-  int buttonState = !digitalRead(pushButton); // 버튼 누르면 '0' 또는 '1'을 buttonState에 저장
+  int buttonState = !digitalRead(pushButton); // 버튼 누르면 '0' 또는 '1'을 변수에 저장
   Serial.println(buttonState);                // '0' 또는 '1' 출력
   delay(1);                                   // 0.001초 기다림
   
